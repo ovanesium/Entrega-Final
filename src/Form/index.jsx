08357772e43e87aed0f46350562222645235addd
ovanesium <bless.juan@hotmail.com>
2023-03-02 18:55:37 -0300

    form

--- src/Form/index.jsx
+++ src/Form/index.jsx
@@ -1,7 +1,7 @@
 import db from "../../db/firebase-config.js";
 import { collection, addDoc } from "firebase/firestore";
 import { useState } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Container, Flex, Wrap, WrapItem } from "@chakra-ui/react";
 
 const Form = ({ cart }) => {
   const [inputNombre, setInputNombre] = useState("");
@@ -34,51 +34,49 @@ const Form = ({ cart }) => {
 
   return (
     <form onSubmit={createItem}>
-      <Wrap justify="center" mt={10} ml="30%" mr="30%">
-        <WrapItem key={"nombre"}>
-          <input
-            type="text"
-            value={inputNombre}
-            placeholder="Nombre"
-            onChange={(e) => setInputNombre(e.target.value)}
-          />
-        </WrapItem>
-        <WrapItem key={"apellido"}>
-          <input
-            type="text"
-            value={inputApellido}
-            placeholder="Apellido"
-            onChange={(e) => setInputApellido(e.target.value)}
-          />
-        </WrapItem>
-        <WrapItem key={"telefono"}>
-          <input
-            type="text"
-            value={inputTelefono}
-            placeholder="Telefono"
-            onChange={(e) => setInputTelefono(e.target.value)}
-          />
-        </WrapItem>
-        <WrapItem key={"email"}>
-          <input
-            type="email"
-            value={inputEmail}
-            placeholder="email"
-            onChange={(e) => setInputEmail(e.target.value)}
-          />
-        </WrapItem>
-        <WrapItem key={"reEmail"}>
-          <input
-            type="email"
-            value={inputReEmail}
-            placeholder="repetir email"
-            onChange={(e) => setInputReEmail(e.target.value)}
-          />
-        </WrapItem>
-        <WrapItem key={"buttonsubmit"}>
-          <button type="submit">Agregar ítem</button>
-        </WrapItem>
-      </Wrap>
+      <Flex justify={"center"} key={"nombre"} mt="20">
+        <input
+          type="text"
+          value={inputNombre}
+          placeholder="Nombre*"
+          onChange={(e) => setInputNombre(e.target.value)}
+        />
+      </Flex>
+      <Flex justify={"center"} key={"apellido"}>
+        <input
+          type="text"
+          value={inputApellido}
+          placeholder="Apellido*"
+          onChange={(e) => setInputApellido(e.target.value)}
+        />
+      </Flex>
+      <Flex justify={"center"} key={"telefono"}>
+        <input
+          type="text"
+          value={inputTelefono}
+          placeholder="Telefono*"
+          onChange={(e) => setInputTelefono(e.target.value)}
+        />
+      </Flex>
+      <Flex justify={"center"} key={"email"}>
+        <input
+          type="email"
+          value={inputEmail}
+          placeholder="email*"
+          onChange={(e) => setInputEmail(e.target.value)}
+        />
+      </Flex>
+      <Flex justify={"center"} key={"reEmail"}>
+        <input
+          type="email"
+          value={inputReEmail}
+          placeholder="repetir email*"
+          onChange={(e) => setInputReEmail(e.target.value)}
+        />
+      </Flex>
+      <Flex justify={"center"} key={"buttonsubmit"} mb="15" mt="15">
+        <button type="submit">FINALIZAR COMPRA</button>
+      </Flex>
     </form>
   );
 };
